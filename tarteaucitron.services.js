806e9d33bdeacb1f41508a9a0ad6ff31d7e9f6a7
Amauri CHAMPEAUX <amauri.champeaux@gmail.com>
2015-02-18 17:30:07 +0100

    Facebook comment support

--- tarteaucitron.services.js
+++ tarteaucitron.services.js
@@ -192,13 +192,33 @@ tarteaucitron.services.facebook = {
     "cookies": [],
     "js": function () {
         "use strict";
-        tarteaucitron.fallback(['fb-post', 'fb-follow', 'fb-comments', 'fb-activity', 'fb-like-box', 'fb-send', 'fb-share-button', 'fb-like'], '');
+        tarteaucitron.fallback(['fb-post', 'fb-follow', 'fb-activity', 'fb-like-box', 'fb-send', 'fb-share-button', 'fb-like'], '');
         tarteaucitron.addScript('//connect.facebook.net/' + tarteaucitron.getLocale() + '/sdk.js#xfbml=1&version=v2.0', 'facebook-jssdk');
     },
     "fallback": function () {
         "use strict";
         var id = 'facebook';
-        tarteaucitron.fallback(['fb-post', 'fb-follow', 'fb-comments', 'fb-activity', 'fb-like-box', 'fb-send', 'fb-share-button', 'fb-like'], tarteaucitron.engage(id));
+        tarteaucitron.fallback(['fb-post', 'fb-follow', 'fb-activity', 'fb-like-box', 'fb-send', 'fb-share-button', 'fb-like'], tarteaucitron.engage(id));
+    }
+};
+
+// facebookcomment
+tarteaucitron.services.facebookcomment = {
+    "key": "facebookcomment",
+    "type": "comment",
+    "name": "Facebook (commentaire)",
+    "uri": "https://www.facebook.com/help/cookies/",
+    "needConsent": true,
+    "cookies": [],
+    "js": function () {
+        "use strict";
+        tarteaucitron.fallback(['fb-comments'], '');
+        tarteaucitron.addScript('//connect.facebook.net/' + tarteaucitron.getLocale() + '/sdk.js#xfbml=1&version=v2.0', 'facebook-jssdk');
+    },
+    "fallback": function () {
+        "use strict";
+        var id = 'facebookcomment';
+        tarteaucitron.fallback(['fb-comments'], tarteaucitron.engage(id));
     }
 };
 
