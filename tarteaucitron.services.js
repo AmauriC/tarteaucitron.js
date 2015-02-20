de2e0fc92cb64388c4368faf1a3bc17caf23549e
Amauri CHAMPEAUX <amauri@ferank.fr>
2015-02-20 23:15:59 +0100

    Update the list of the Google Tag Manager cookies

--- tarteaucitron.services.js
+++ tarteaucitron.services.js
@@ -467,7 +467,7 @@ tarteaucitron.services.googletagmanager = {
     "name": "Google Tag Manager",
     "uri": "http://www.google.com/ads/preferences/",
     "needConsent": true,
-    "cookies": ['_ga', '_gat', '__utma', '__utmb', '__utmc', '__utmt', '__utmz'],
+    "cookies": ['_ga', '_gat', '__utma', '__utmb', '__utmc', '__utmt', '__utmz', '__gads', '_drt_', 'FLC', 'exchange_uid', 'id', 'fc', 'rrs', 'rds', 'rv', 'uid', 'UIDR', 'UID', 'clid', 'ipinfo', 'acs'],
     "js": function () {
         "use strict";
         if (tarteaucitron.user.googletagmanagerId === undefined) {
