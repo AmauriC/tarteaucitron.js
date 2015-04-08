0f49b7613f9de0e4a01f22cae890af146a183b9c
Amauri CHAMPEAUX <amauri@ferank.fr>
2015-04-08 19:15:35 +0200

    Increment version number

--- tarteaucitron.js
+++ tarteaucitron.js
@@ -10,7 +10,7 @@ var scripts = document.getElementsByTagName('script'),
     tarteaucitronNoAdBlocker = false;
 
 var tarteaucitron = {
-    "version": 206,
+    "version": 207,
     "cdn": cdn,
     "user": {},
     "lang": {},
