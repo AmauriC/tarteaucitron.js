493be4941b2806733ba0e71f795eaa562ad91899
Amauri CHAMPEAUX <amauri@ferank.fr>
2015-05-28 19:26:29 +0200

    Add Timeline JS

--- tarteaucitron.js
+++ tarteaucitron.js
@@ -10,7 +10,7 @@ var scripts = document.getElementsByTagName('script'),
     tarteaucitronNoAdBlocker = false;
 
 var tarteaucitron = {
-    "version": 207,
+    "version": 210,
     "cdn": cdn,
     "user": {},
     "lang": {},
