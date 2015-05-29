f0cb12be1f1fad9e89c5c4f781d1cb32ddfd8584
Amauri CHAMPEAUX <amauri@ferank.fr>
2015-05-29 17:40:33 +0200

    Add Twenga

--- tarteaucitron.js
+++ tarteaucitron.js
@@ -10,7 +10,7 @@ var scripts = document.getElementsByTagName('script'),
     tarteaucitronNoAdBlocker = false;
 
 var tarteaucitron = {
-    "version": 211,
+    "version": 212,
     "cdn": cdn,
     "user": {},
     "lang": {},
