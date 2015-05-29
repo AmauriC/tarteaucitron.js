e9bb89fa0a28cd7a9b2e0305cd49054998aa1dc0
Amauri CHAMPEAUX <amauri@ferank.fr>
2015-05-29 16:24:28 +0200

    Remove the Google Maps Api key

--- tarteaucitron.js
+++ tarteaucitron.js
@@ -10,7 +10,7 @@ var scripts = document.getElementsByTagName('script'),
     tarteaucitronNoAdBlocker = false;
 
 var tarteaucitron = {
-    "version": 210,
+    "version": 211,
     "cdn": cdn,
     "user": {},
     "lang": {},
