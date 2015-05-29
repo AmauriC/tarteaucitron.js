615f8c8d681fca0eaa531c192b12275ff598e728
Amauri CHAMPEAUX <amauri@ferank.fr>
2015-05-29 17:40:16 +0200

    Add Twenga

--- tarteaucitron.services.js
+++ tarteaucitron.services.js
@@ -1031,6 +1031,24 @@ tarteaucitron.services.typekit = {
     }
 };
 
+// twenga
+tarteaucitron.services.twenga = {
+    "key": "twenga",
+    "type": "ads",
+    "name": "Twenga",
+    "uri": "http://www.twenga.com/privacy.php",
+    "needConsent": true,
+    "cookies": [],
+    "js": function () {
+        "use strict";
+        
+        if (tarteaucitron.user.twengaId === undefined || tarteaucitron.user.twengaLocale === undefined) {
+            return;
+        }
+        
+        tarteaucitron.addScript('//tracker.twenga.' + tarteaucitron.user.twengaLocale + '/st/tracker_' + tarteaucitron.user.twengaId + '.js');
+    }
+};
 
 // twitter
 tarteaucitron.services.twitter = {
