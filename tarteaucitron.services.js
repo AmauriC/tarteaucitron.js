a761105ec18f8eabf5a9fb7a971805ee9af3de32
Amauri CHAMPEAUX <amauri@ferank.fr>
2015-05-29 18:19:23 +0200

    Add Crazyegg

--- tarteaucitron.services.js
+++ tarteaucitron.services.js
@@ -215,6 +215,25 @@ tarteaucitron.services.clicmanager = {
     }
 };
 
+// crazyegg
+tarteaucitron.services.crazyegg = {
+    "key": "crazyegg",
+    "type": "analytic",
+    "name": "Crazy Egg",
+    "uri": "https://www.crazyegg.com/privacy",
+    "needConsent": true,
+    "cookies": [],
+    "js": function () {
+        "use strict";
+        
+        if (tarteaucitron.user.crazyeggId === undefined) {
+            return;
+        }
+        
+        tarteaucitron.addScript('//script.crazyegg.com/pages/scripts/' + tarteaucitron.user.crazyeggId.substr(0, 4) + '/' + tarteaucitron.user.crazyeggId.substr(4, 4) + '.js');
+    }
+};
+
 // criteo
 tarteaucitron.services.criteo = {
     "key": "criteo",
