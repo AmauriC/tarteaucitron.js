827bbe6a6256343065409f45f1f1c4a3d658c6cc
Fabien Leprovost <oxynel@me.com>
2018-05-21 15:07:09 +0200

    [ADD] olark chat service

--- tarteaucitron.services.js
+++ tarteaucitron.services.js
@@ -1848,3 +1848,23 @@ tarteaucitron.services.facebookpixel = {
     }
 };
 
+// olark
+
+tarteaucitron.services.olark = {
+    "key": "olark",
+    "type": "social",
+    "name": "olark",
+    "uri": "https://www.olark.com",
+    "needConsent": true,
+    "cookies": ['_ok', '_okbk','_okdetect','_oklv','olfsk'],
+    "js": function () {
+        "use strict";
+        if (tarteaucitron.user.olarkId === undefined) {
+            return;
+        }
+        
+(function(o,l,a,r,k,y){if(o.olark)return; r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0]; y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r); y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)}; y.extend=function(i,j){y("extend",i,j)}; y.identify=function(i){y("identify",k.i=i)}; y.configure=function(i,j){y("configure",i,j);k.c[i]=j}; k=y._={s:[],t:[+new Date],c:{},l:a}; })(window,document,"static.olark.com/jsclient/loader.js");olark.identify(tarteaucitron.user.olarkId);
+
+        
+    },
+};
