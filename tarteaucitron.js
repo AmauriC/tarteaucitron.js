263fa2fd4696562f18a7b7eee63e90d87809b6f1
Encre Informatique <j.devergnies@outlook.com>
2018-05-22 12:26:07 -0300

    Fixed #93 : dutch translation
    The Dutch translation has been added according to the issue.
    Beware I cannot check the translation itself.
    The nl locale has been added to the main script.

--- tarteaucitron.js
+++ tarteaucitron.js
@@ -1040,7 +1040,7 @@ var tarteaucitron = {
         "use strict";
         if (!navigator) { return 'en'; }
         
-        var availableLanguages = 'cs,en,fr,es,it,de,pt,pl,ru',
+        var availableLanguages = 'cs,en,fr,es,it,de,nl,pt,pl,ru',
             defaultLanguage = 'en',
             lang = navigator.language || navigator.browserLanguage ||
                 navigator.systemLanguage || navigator.userLang || null,
@@ -1077,6 +1077,8 @@ var tarteaucitron = {
             return 'it_IT';
         } else if (userLanguage === 'pt') {
             return 'pt_PT';
+        } else if (userLanguage === 'nl') {
+            return 'nl_NL';
         } else {
             return 'en_US';
         }
