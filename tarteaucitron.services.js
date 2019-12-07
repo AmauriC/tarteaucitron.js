c6b1a248543c43fcdf1d8abcb9c7039479530049
Amauri CHAMPEAUX <amauri@ferank.fr>
2019-12-07 11:18:52 +0100

    Fix Yandex Metrica

--- tarteaucitron.services.js
+++ tarteaucitron.services.js
@@ -63,16 +63,16 @@ tarteaucitron.services.metrica = {
         }
         tarteaucitron.addScript('https://mc.yandex.ru/metrika/tag.js', '', function() {
 
-            window.ym = window.ym || function () {
-                (window["ym"].a = window["ym"].a || []).push(arguments)
-            };
-            window["ym"].l = 1 * new Date();
+           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            window.ym(tarteaucitron.user.yandexmetrica, "init", {
-                clickmap: true,
-                trackLinks: true,
-                accurateTrackBounce: true,
-                webvisor: true
+           ym(tarteaucitron.user.yandexmetrica, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true,
+                ecommerce:"dataLayer"
             });
         });
     }
