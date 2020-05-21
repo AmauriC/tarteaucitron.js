dcbc7399d58d3a7c40cce7b090f189fc202979ae
Amauri CHAMPEAUX <amauri@champeaux.fr>
2020-05-21 14:29:11 +0200

    enable sweden locale

--- tarteaucitron.js
+++ tarteaucitron.js
@@ -1374,7 +1374,7 @@ var tarteaucitron = {
     "getLanguage": function () {
         "use strict";
 
-        var availableLanguages = 'bg,cn,cs,da,de,el,en,es,fi,fr,hu,it,ja,nl,oc,pl,pt,ro,ru,sk,tr,vi',
+        var availableLanguages = 'bg,cn,cs,da,de,el,en,es,fi,fr,hu,it,ja,nl,oc,pl,pt,ro,ru,se,sk,tr,vi',
             defaultLanguage = 'en';
 
         if (tarteaucitronForceLanguage !== '') {
