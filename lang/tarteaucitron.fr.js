260db019d9fdf24eedf8ad8bcc40302863827945
gregoweb <gregoweb@user.noreply.github.com>
2020-12-11 13:35:13 +0100

    Replace "ce" by "ceux" explanation : "vous donne le contrôle sur ces cookies : ceux-ci"

--- lang/tarteaucitron.fr.js
+++ lang/tarteaucitron.fr.js
@@ -10,7 +10,7 @@ tarteaucitron.lang = {
     "alertBigClick": "En poursuivant votre navigation,",
     "alertBig": "vous acceptez l'utilisation de services tiers pouvant installer des cookies",
     
-    "alertBigPrivacy": "Ce site utilise des cookies et vous donne le contrôle sur ce que vous souhaitez activer",
+    "alertBigPrivacy": "Ce site utilise des cookies et vous donne le contrôle sur ceux que vous souhaitez activer",
     "alertSmall": "Gestion des services",
     "acceptAll": "OK, tout accepter",
     "personalize": "Personnaliser",
