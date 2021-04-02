b758e809cebeb47d1c1a5c4fb57f27037706ef10
jessicakuijer <62114740+jessicakuijer@users.noreply.github.com>
2021-04-02 10:03:44 +0200

    Update tarteaucitron.fr.js
    rajout de traduction manquante pour les acceptation ou refus.
    ****
    I've just added some missing translation for allowAll/denyAll/acceptAll

--- lang/tarteaucitron.fr.js
+++ lang/tarteaucitron.fr.js
@@ -1,7 +1,5 @@
 /*global tarteaucitron */
 tarteaucitron.lang = {
-
-    "middleBarHead": "☝ 🍪",
     "adblock": "Bonjour! Ce site joue la transparence et vous donne le choix des services tiers à activer.",
     "adblock_call": "Merci de désactiver votre adblocker pour commencer la personnalisation.",
     "reload": "Recharger la page",
@@ -10,15 +8,15 @@ tarteaucitron.lang = {
     "alertBigClick": "En poursuivant votre navigation,",
     "alertBig": "vous acceptez l'utilisation de services tiers pouvant installer des cookies",
     
-    "alertBigPrivacy": "Ce site utilise des cookies et vous donne le contrôle sur ceux que vous souhaitez activer",
+    "alertBigPrivacy": "Ce site utilise des cookies et vous donne le contrôle sur ce que vous souhaitez activer",
     "alertSmall": "Gestion des services",
-    "acceptAll": "Tout accepter",
+    "acceptAll": "OK, tout accepter",
+    "denyAll": "Tout refuser",
+    "allowAll": "Tout accepter",
     "personalize": "Personnaliser",
     "close": "Fermer",
 
-    "privacyUrl": "Politique de confidentialité",
-
-    "all": "Préférences pour tous les services",
+    "all": "Préférence pour tous les services",
 
     "info": "Protection de votre vie privée",
     "disclaimer": "En autorisant ces services tiers, vous acceptez le dépôt et la lecture de cookies et l'utilisation de technologies de suivi nécessaires à leur bon fonctionnement.",
@@ -31,17 +29,6 @@ tarteaucitron.lang = {
     "more": "En savoir plus",
     "source": "Voir le site officiel",
     "credit": "Gestion des cookies par tarteaucitron.js",
-    "noServices": "Ce site n'utilise aucun cookie nécessitant votre consentement.",
-
-    "toggleInfoBox": "Afficher/masquer les informations sur le stockage des cookies",
-    "title": "Panneau de gestion des cookies",
-    "cookieDetail": "Détail des cookies",
-    "ourSite": "sur notre site",
-    "newWindow": "(nouvelle fenêtre)",
-    "allowAll": "Tout accepter",
-    "denyAll": "Tout refuser",
-
-    "icon": "Cookies",
     
     "fallback": "est désactivé.",
 
@@ -76,8 +63,5 @@ tarteaucitron.lang = {
     "other": {
         "title": "Autre",
         "details": "Services visant à afficher du contenu web."
-    },
-    
-    "mandatoryTitle": "Cookies obligatoires",
-    "mandatoryText": "Ce site utilise des cookies nécessaires à son bon fonctionnement. Ils ne peuvent pas être désactivés."
+    }
 };
