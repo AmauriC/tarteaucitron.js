797fa033571ed9536e2774d3a8636116df3f2cb0
Marc Bellêtre <marc@pixelparfait.fr>
2021-04-12 17:18:41 +0200

    Revert iframe title

--- tarteaucitron.services.js
+++ tarteaucitron.services.js
@@ -1832,7 +1832,7 @@ tarteaucitron.services.instagram = {
     "js": function () {
         "use strict";
         tarteaucitron.fallback(['instagram_post'], function (x) {
-            var post_id = x.getAttribute('postId'),
+            var frame_title = tarteaucitron.fixSelfXSS(x.getAttribute("title") || 'Instagram iframe'),
                 post_permalink = x.getAttribute('data-instgrm-permalink'),
                 embed_width = x.getAttribute('width'),
                 embed_height = x.getAttribute('height'),
@@ -1861,7 +1861,7 @@ tarteaucitron.services.instagram = {
                 frame_height = '"" ';
             }
 
-            post_frame = '<iframe title="Instagram iframe" src="//www.instagram.com/' + post_id + '/embed" ' + frame_width + frame_height + '></iframe>';
+            post_frame = '<iframe title="' + frame_title + '" src="//www.instagram.com/' + post_id + '/embed" ' + frame_width + frame_height + '></iframe>';
 
             return post_frame;
         });
