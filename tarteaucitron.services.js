f7d906a8a804450dee18d411d38d8c5c31f5011e
Amauri CHAMPEAUX <amauri@champeaux.fr>
2022-03-14 11:00:38 +0100

    Add Crisp #906

--- tarteaucitron.services.js
+++ tarteaucitron.services.js
@@ -5095,3 +5095,25 @@ tarteaucitron.services.gallica = {
     }
 };
 
+// crisp
+tarteaucitron.services.crisp = {
+    "key": "crisp",
+    "type": "other",
+    "name": "Crisp Chat",
+    "uri": "https://help.crisp.chat/en/article/crisp-chatbox-cookie-ip-policy-1147xor/",
+    "needConsent": false,
+    "cookies": ['crisp-client', '__cfduid'],
+    "js": function () {
+        "use strict";
+
+        if (tarteaucitron.user.crispID === undefined) {
+            return;
+        }
+
+        window.$crisp = [];
+        window.CRISP_WEBSITE_ID = tarteaucitron.user.crispID;
+
+        tarteaucitron.addScript('https://client.crisp.chat/l.js');
+    }
+};
+
