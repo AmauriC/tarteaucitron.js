7da0344b93b1abfae5290a8f24bd1f318884b008
Amauri CHAMPEAUX <amauri@champeaux.fr>
2022-10-18 16:43:23 +0200

    Update sv/se locale

--- lang/tarteaucitron.se.js
+++ lang/tarteaucitron.se.js
@@ -25,7 +25,7 @@ tarteaucitron.lang = {
     "deny": "Förneka",
     "noCookie": "Den här tjänsten använder inte cookie.",
     "useCookie": "Den här tjänsten kan installeras",
-    "useCookieCurrent": "Den här tjänsten har installerats",
+    "useCookieCurrent": "Den här tjänsten har installerat",
     "useNoCookie": "Den här tjänsten har inte installerat någon cookie.",
     "more": "Läs mer",
     "source": "Visa den officiella webbplatsen",
