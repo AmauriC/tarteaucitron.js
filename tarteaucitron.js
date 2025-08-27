0462c06f3b8b9dfc3dc0e05272e9473883c5a642
Amauri <amauri@champeaux.fr>
2025-08-27 18:44:47 +0200

    Bump version to 1.24.0

--- tarteaucitron.js
+++ tarteaucitron.js
@@ -22,7 +22,7 @@ var tarteaucitronScriptsDiscover = document.getElementsByTagName('script'),
 
 
 var tarteaucitron = {
-    "version": "1.23.0",
+    "version": "1.24.0",
     "cdn": cdn,
     "user": {},
     "lang": {},
