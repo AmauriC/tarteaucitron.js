c1f8920b52bda91edfdb7eb884a9a66aad84b00c
Amauri <amauri@champeaux.fr>
2026-09-22 15:29:47 +0200

    Add Statable Fix #1414

--- tarteaucitron.services.js
+++ tarteaucitron.services.js
@@ -35,6 +35,29 @@ tarteaucitron.services.iframe = {
     }
 };
 
+// statable
+tarteaucitron.services.statable = {
+    "key": "statable",
+    "type": "analytic",
+    "name": "Statable",
+    "uri": "https://statable.com/privacy",
+    "needConsent": false,
+    "cookies": [],
+    "js": function () {
+        "use strict";
+
+        if (tarteaucitron.user.statableSiteId === undefined) {
+            return;
+        }
+
+        if (tarteaucitron.user.statableHost === undefined) {
+            tarteaucitron.user.statableHost = 'statable.com';
+        }
+
+        tarteaucitron.addScript('https://' + tarteaucitron.user.statableHost + '/js/' + tarteaucitron.user.statableSiteId + '/s.js');
+    }
+};
+
 // brevochat
 tarteaucitron.services.brevochat = {
     "key": "brevochat",
