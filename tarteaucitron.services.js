e7bcb33fc2067d9c55965519698d06d370ce8779
Amauri <amauri@champeaux.fr>
2026-09-24 15:47:10 +0200

    Switch needConsent for Crisp to true

--- tarteaucitron.services.js
+++ tarteaucitron.services.js
@@ -7258,7 +7258,7 @@ tarteaucitron.services.crisp = {
     "type": "support",
     "name": "Crisp Chat",
     "uri": "https://help.crisp.chat/en/article/crisp-chatbox-cookie-ip-policy-1147xor/",
-    "needConsent": false,
+    "needConsent": true,
     "cookies": ['crisp-client', '__cfduid'],
     "js": function () {
         "use strict";
